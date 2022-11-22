ba3c60a24d49bf6b2083add03a78e6866c1e92b9
toce111 <nestorovantonio@icloud.com>
2022-11-22 01:17:50 +0100

    03 - Statements Homework

---
+++ Homeworks/Javascript Homework/03/js/style.js
@@ -0,0 +1,96 @@
+// THE HELLO WORLD EXERCISE (if statement)
+
+var code = (prompt("Enter the language you want to display Hello World: \nes - espanyol \nde - german \nen - english\nmk - macedonian\n "));
+if(code == "es") {
+    console.log("Hola Mundo!");
+    document.write("Hola Mundo!");
+} else if(code == "de") {
+    console.log("Hallo Welt!");
+    document.write("Hallo Welt!");
+} else if(code == "en") {
+    console.log("Hello World!");
+    document.write("Hello World!");
+} else if(code == "mk") {
+    console.log("Zdravo svetu?!");
+    document.write("Zdravo svetu?!");
+} else {
+    console.log("Check your spelling and try again.");
+    document.write("Check your spelling and try again.");
+};
+
+// SEASONS (if statement)
+
+var seasons = prompt("Enter any month of the year and i will tell you the season: ")
+if (seasons === "september" || seasons === "october" || seasons === "november"){
+    console.log("The season is autumn.");
+    alert("\nThe season is autumn.\n");
+} else if (seasons === "december" || seasons === "january" || seasons === "february"){
+    console.log("The season is winter.");
+    alert("\nThe season is winter.\n");
+} else if (seasons === "march" || seasons === "april" || seasons === "may"){
+    console.log("The season is spring.");
+    alert("\nThe season is spring.\n");
+} else if (seasons === "june" || seasons === "july" || seasons === "august"){
+    console.log("The season is summer.");
+    alert("\nThe season is summer.\n");
+} else {
+    console.log("\nPlease check your spelling make sure everything is smaller lowercase and try again.\n");
+    alert("\nPlease check your spelling make sure everything is smaller lowercase and try again.\n");
+};
+
+// MUSICIANS (switch statement)
+
+var band_members_num = prompt("Enter the number to check what kind of a music group it is:");
+switch (band_members_num) {
+    case "1":
+        console.log("The band is uno.");
+        alert("The band is uno.");
+        document.write("The band is uno.");
+    break;
+    case "2":
+        console.log("The band is dos.");
+        alert("The band is dos.");
+        document.write("The band is dos.");
+    break;
+    case "3":
+        console.log("The band is tres.");
+        alert("The band is tres.");
+        document.write("The band is tres.");
+    break;
+    case "4":
+        console.log("The band is cuatro.");
+        alert("The band is cuatro.");
+        document.write("The band is cuatro.");
+    break;
+    case "5":
+        console.log("The band is cinco.");
+        alert("The band is cinco.");
+        document.write("The band is cinco.");
+    break;
+    case "6":
+        console.log("The band is seis.");
+        alert("The band is seis.");
+        document.write("The band is seis.");
+    break;
+    case "7":
+        console.log("The band is siete.");
+        alert("The band is siete.");
+        document.write("The band is siete.");
+    break;
+    case "8":
+        console.log("The band is ocho.");
+        alert("The band is ocho.");
+        document.write("The band is ocho.");
+    break;
+    case "9":
+        console.log("The band is nueve.");
+        alert("The band is nueve.");
+        document.write("The band is nueve.");
+    break;
+    case "10":
+        console.log("The band is diez.");
+        alert("The band is diez.");
+        document.write("The band is diez.");
+    break;
+    default: alert("Check your spelling maybe your number is above 10.");
+}
