78b625ca618faea5223ce06658e6a828617b3f71
Antonio <nestorovantonio@icloud.com>
2022-11-23 21:45:58 +0100

    04 - Loops

---
+++ Javascript/04/js/script.js
@@ -0,0 +1,60 @@
+// Loops
+
+// for loops
+// for (var i = 0; i <= 10; i++) {
+//     console.log(`The index is at number: ${i}`)
+// }
+
+// for (var i = 0; i <= 100; i+=5) {
+//     console.log(`The index is at number: ${i}`)
+// }
+
+// for (var i = 100; i >= 0; i--) {
+//     console.log(`The index is at number: ${i}`)
+// }
+
+// var movies = [
+//     {name: "Pulp Fiction", year: 1994},
+//     {name: "Mad Max", year: 1982},
+//     {name: "8 Mile", year: 2002},
+//     {name: "Cold War", year: 1954},
+// ];
+
+// console.log(movies);
+// console.log(movies[0]);
+// console.log(movies[1]);
+// console.log(movies[2]);
+
+// for(var i = 0; i < movies.length; i++) {
+//     // console.log(`Index number ${i}`);
+//     // console.log(movies[i].name, movies[i].year);
+//     if(movies[i].year === 1954) {
+//         console.log(movies[i]);
+//     }
+// }
+
+// var numbers = [15, 192, 14, 84, 883, 74, 92, 12, 74, 56];
+// var result = 0;
+
+// for(var i = 0; i < numbers.length; i++) {
+//     result += numbers[i];
+// }
+
+// console.log(`The sum of the numbers array is ${result}`);
+
+// var other_numbers = [
+//     [5, 10, 15, 20],
+//     [40, 50, 60, 70],
+//     [100, 200, 300, 400]
+// ];
+
+// var result = 0;
+
+// for(var i = 0; i < other_numbers.length; i++) {
+//     for(var j = 0; j < other_numbers[i].length; j++) {
+//         // console.log(other_numbers[i][j]);
+//         result += other_numbers[i][j];
+//     }
+// }
+
+// console.log(result);
